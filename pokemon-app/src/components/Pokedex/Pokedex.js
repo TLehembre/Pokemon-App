9dc1e0387c990ae851d7519dfebae52f4adb412a
TLehembre <toon.lehembre@gmail.com>
2022-10-18 16:21:06 +0200

    Add a list of pokemon names

--- pokemon-app/src/components/Pokedex/Pokedex.js
+++ pokemon-app/src/components/Pokedex/Pokedex.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
 
-export default class Pokedex extends React.Component {
-  render() {
-    return <h1>Hello</h1>;
-  }
+function Pokedex() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get('https://stoplight.io/mocks/appwise-be/pokemon/57519009/pokemon')
+    .then(res => {
+      console.log(res)
+      setPokemons(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }, [])
+
+  return (
+    <>
+      <div>Pokedex</div>
+      <div>
+        <ul>
+          {
+            pokemons.map(pokemon => <li key={pokemon.id}>{pokemon.name}</li>)
+          }
+        </ul>
+      </div>
+    </>
+  )
 }
+
+export default Pokedex
