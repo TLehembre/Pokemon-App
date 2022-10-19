d934a53e0f5d79b343f25d36b9f4119b8d4167c4
TLehembre <toon.lehembre@gmail.com>
2022-10-19 15:26:47 +0200

    Update list with more pokemon data

--- pokemon-app/src/components/Pokedex/Pokedex.js
+++ pokemon-app/src/components/Pokedex/Pokedex.js
@@ -1,5 +1,9 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Pokedex.css';
+import { Button } from 'react-bootstrap';
+
 
 function Pokedex() {
   const [pokemons, setPokemons] = useState([]);
@@ -18,11 +22,20 @@ function Pokedex() {
 
   return (
     <>
-      <div>Pokedex</div>
+      <h1>Pokédex</h1>
+      <input placeholder='search'></input>
+      <Button>My team</Button>
+      <Button>Favorites</Button>
       <div>
-        <ul>
+        <ul className='list-group'>
           {
-            pokemons.map(pokemon => <li key={pokemon.id}>{pokemon.name}</li>)
+            pokemons.map(pokemon => <li className='list-group-item' key={pokemon.id}>
+                <div className='pokemon-list-item'>
+                  <img alt={pokemon.name} src={pokemon.sprites.front_default} className='pokemon-image flex-item'/>
+                   <div className='flex-item pokemon-id-item'><p className='pokemon-name'>{pokemon.name}</p><p className='pokemon-id'>Nr. {pokemon.id}</p></div>
+                   <div className='flex-item'>dfdffds</div>
+                  </div>
+              </li>)
           }
         </ul>
       </div>
