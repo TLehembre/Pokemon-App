186e5a31cec0e06f958a51049e95ad27ac6ecfdc
TLehembre <toon.lehembre@gmail.com>
2022-10-20 21:21:18 +0200

    Update list of pokemons with types

--- pokemon-app/src/components/Pokedex/Pokedex.js
+++ pokemon-app/src/components/Pokedex/Pokedex.js
@@ -4,6 +4,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './Pokedex.css';
 import { Button } from 'react-bootstrap';
 
+function getTypeColor(type) {
+  let color;
+
+  switch(type) {
+    case 'grass': color = '#95C24D';
+    break;
+    case 'fire': color = '#FD7D25';
+    break;
+    case 'water': color = '#4592C3';
+    break;
+    case 'poison': color = '#BA7EC8';
+    break;
+    case 'electric': color = 'yellow';
+    break;
+    case 'flying': color = 'red';
+    break;
+    case 'bug': color = 'darkgreen';
+    break;
+    case 'rock': color = 'gray';
+    break;
+    case 'psychic': color = 'pink';
+    break;
+    case 'normal': color = '#A3ACAE';
+    break;
+    case 'ghost': color = '#7B62A3';
+    break;
+    default: color = 'black';
+  }
+
+    return {backgroundColor: color}
+  
+}
 
 function Pokedex() {
   const [pokemons, setPokemons] = useState([]);
@@ -33,7 +65,12 @@ function Pokedex() {
                 <div className='pokemon-list-item'>
                   <img alt={pokemon.name} src={pokemon.sprites.front_default} className='pokemon-image flex-item'/>
                    <div className='flex-item pokemon-id-item'><p className='pokemon-name'>{pokemon.name}</p><p className='pokemon-id'>Nr. {pokemon.id}</p></div>
-                   <div className='flex-item'>dfdffds</div>
+                   <div className='types flex-item'>
+                      {
+                        pokemon.types.map(type => 
+                            <p className='tag' style={getTypeColor(type.type.name)} key={type.slot}>{type.type.name}</p>)
+                      }
+                    </div>
                   </div>
               </li>)
           }
